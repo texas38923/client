e0645aaba5ab205bff0d1e6cf9cb33a6efcba4db
Anurag Kamra <anuragkamra7@gmail.com>
2024-04-02 23:05:53 +0530

    some more basic framework changes

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = () => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+    </React.Fragment>
+  );
+};
+
+export default Posts;
