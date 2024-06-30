3ac7e7b0e044ae5d809f27ec873aa93b520aad18
Anurag Kamra <anuragkamra7@gmail.com>
2024-06-30 16:17:48 +0530

    completed create, update, like, delete functionalities

---
+++ src/reducers/posts.js
@@ -0,0 +1,28 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from '../constants/actionTypes';
+
+//state --> posts -> has to be a default value, as cannot be null;
+const reducer = (posts = [], action) => {
+  switch (action.type) {
+    case DELETE:
+      return posts.filter((post) => post._id !== action.payload);
+    case UPDATE:
+    case LIKE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
